93e580d07a1110e6ac311446e63c35ff90c59608
zhcan <zcan114@126.com>
2024-10-23 14:14:40 +0800

    remove QSysInfo

--- sdk/rmsauth_sdk/rmsauth/RmsauthIdHelperQt.cpp
+++ sdk/rmsauth_sdk/rmsauth/RmsauthIdHelperQt.cpp
@@ -7,26 +7,29 @@
 */
 
 #include <RmsauthIdHelper.h>
-#include <QSysInfo>
-#include <QString>
+// #include <QSysInfo>
+// #include <QString>
+#include <string>
 
 namespace rmsauth {
 
 const String RmsauthIdHelper::getProcessorArchitecture()
 {
-#ifdef QT_VER_LESS_THEN_54
-    return "unk";
-#else
-    return QSysInfo::currentCpuArchitecture().toStdString();
-#endif
+// #ifdef QT_VER_LESS_THEN_54
+//     return "unk";
+// #else
+//     return QSysInfo::currentCpuArchitecture().toStdString();
+// #endif
+    return std::string("arm64");
 }
 const String RmsauthIdHelper::getOSVersion()
 {
-#ifdef QT_VER_LESS_THEN_54
-    return "unk";
-#else
-    return QSysInfo::kernelType().toStdString();
-#endif
+// #ifdef QT_VER_LESS_THEN_54
+//     return "unk";
+// #else
+//     return QSysInfo::kernelType().toStdString();
+// #endif
+    return std::string("HongMeng");
 }
 
 } // namespace rmsauth {
