5bd1ff99865fe710fc87b39aaa36ae19c5488fa6
Ryan <709231541@qq.com>
2024-10-26 15:05:29 +0800

    remove Qt in UserRealmDiscoveryResponseQt

--- sdk/rmsauth_sdk/rmsauth/UserRealmDiscoveryResponseQt.cpp
+++ sdk/rmsauth_sdk/rmsauth/UserRealmDiscoveryResponseQt.cpp
@@ -9,14 +9,14 @@
 #include <UserRealmDiscoveryResponse.h>
 #include <Logger.h>
 #include <RmsauthIdHelper.h>
-#include <QNetworkRequest>
-#include <QCoreApplication>
+// #include <QNetworkRequest>
+// #include <QCoreApplication>
 #include <nlohmann/json.hpp>
 #include "HttpHelperQt.h"
 #include "JsonUtilsQt.h"
 
 namespace rmsauth {
-
+#if 0
 static UserRealmDiscoveryResponse deserializeUserRealmDiscoveryResponse(const QByteArray& body)
 {
     Logger::info("deserializeUserRealmDiscoveryResponse", "jsonObject: %", String(body.begin(), body.end()));
@@ -66,5 +66,47 @@ UserRealmDiscoveryResponse UserRealmDiscoveryResponse::createByDiscoveryAsync(co
     auto userRealmResponse = deserializeUserRealmDiscoveryResponse(body);
     return std::move(userRealmResponse);
 }
+#endif
+static UserRealmDiscoveryResponse deserializeUserRealmDiscoveryResponse(const String& body)
+{
+    Logger::info("deserializeUserRealmDiscoveryResponse", "jsonObject: %", body);
 
+    nlohmann::json qobj;
+    try {
+        qobj = nlohmann::json::parse(body);
+    }
+    catch(std::exception& e)
+    {
+        throw RmsauthException(String("deserializeUserRealmDiscoveryResponse: ") + e.what());
+    }
+
+    UserRealmDiscoveryResponse userRealmResponse;
+
+    userRealmResponse.version(JsonUtilsQt::getStringOrDefault(qobj, UserRealmDiscoveryResponse::jsonNames().version));
+    userRealmResponse.accountType(JsonUtilsQt::getStringOrDefault(qobj, UserRealmDiscoveryResponse::jsonNames().accountType));
+    userRealmResponse.federationProtocol(JsonUtilsQt::getStringOrDefault(qobj, UserRealmDiscoveryResponse::jsonNames().federationProtocol));
+    userRealmResponse.federationMetadataUrl(JsonUtilsQt::getStringOrDefault(qobj, UserRealmDiscoveryResponse::jsonNames().federationMetadataUrl));
+    userRealmResponse.federationActiveAuthUrl(JsonUtilsQt::getStringOrDefault(qobj, UserRealmDiscoveryResponse::jsonNames().federationActiveAuthUrl));
+
+    return userRealmResponse;
+}
+
+UserRealmDiscoveryResponse UserRealmDiscoveryResponse::createByDiscoveryAsync(const String& userRealmUri, const String& userName, CallStatePtr callState)
+{
+    String userRealmEndpoint = userRealmUri + userName + "?api-version=1.0";
+
+    Logger::info(Tag(), "Sending user realm discovery request to '%'", userRealmEndpoint);
+
+    Headers headers = RmsauthIdHelper::getPlatformHeaders();
+    Headers productHeaders = RmsauthIdHelper::getProductHeaders();
+    headers.insert(productHeaders.begin(), productHeaders.end());
+    headers["Accept"] = "application/json";
+
+    auto fut = std::async(std::launch::async, [&]() {
+        return HttpHelperQt::jobGet(userRealmEndpoint, headers, callState);
+    });
+    auto body = fut.get();
+    auto userRealmResponse = deserializeUserRealmDiscoveryResponse(body);
+    return userRealmResponse;
+}
 } // namespace rmsauth {
