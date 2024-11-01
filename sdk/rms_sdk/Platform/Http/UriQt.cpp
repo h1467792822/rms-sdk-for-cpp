95c6804e0abfdf82977889166d9b8cb0fe03031b
Ryan <709231541@qq.com>
2024-11-01 15:01:47 +0800

    remove qt in uriQt.cpp

--- sdk/rms_sdk/Platform/Http/UriQt.cpp
+++ sdk/rms_sdk/Platform/Http/UriQt.cpp
@@ -8,8 +8,11 @@
 
 #ifdef QTFRAMEWORK
 #include "UriQt.h"
+#include <regex>
+
 namespace rmscore { namespace platform { namespace http {
 
+#if 0
 std::shared_ptr<IUri> IUri::Create(const std::string& uri)
 {
     return std::make_shared<UriQt>(uri);
@@ -29,6 +32,54 @@ int UriQt::GetPort() const
 const std::string UriQt::ToString()const
 {
     return this->pImpl_->toString().toStdString();
+}
+#endif
+
+std::shared_ptr<IUri> IUri::Create(const std::string& uri)
+{
+    return std::make_shared<UriQt>(uri);
+}
+
+UriQt::UriQt(const std::string& uri) : uri_(uri), port_(-1) {
+    // 使用正则表达式解析URI
+    std::regex uri_regex("^(?:([^:/?#]+):)?(?://([^/?#]*))?");
+    std::smatch matches;
+    
+    if (std::regex_search(uri, matches, uri_regex)) {
+        if (matches.size() > 1) scheme_ = matches[1];
+        if (matches.size() > 2) {
+            std::string authority = matches[2];
+            auto pos = authority.find(':');
+            if (pos != std::string::npos) {
+                host_ = authority.substr(0, pos);
+                try {
+                    port_ = std::stoi(authority.substr(pos + 1));
+                } catch (...) {
+                    port_ = -1;
+                }
+            } else {
+                host_ = authority;
+            }
+        }
+    }
+}
+
+UriQt::~UriQt() = default;
+
+const std::string UriQt::GetScheme() const {
+    return scheme_;
+}
+
+const std::string UriQt::GetHost() const {
+    return host_;
+}
+
+int UriQt::GetPort() const {
+    return port_;
+}
+
+const std::string UriQt::ToString() const {
+    return uri_;
 }
 
 }}} // namespace rmscore { namespace platform { namespace http {
--- sdk/rms_sdk/Platform/Http/UriQt.h
+++ sdk/rms_sdk/Platform/Http/UriQt.h
@@ -10,15 +10,15 @@
 #define IURIQTIMPL
 
 #include "IUri.h"
-#include "QUrl"
+// #include "QUrl"
 
 namespace rmscore { namespace platform { namespace http {
 
 class UriQt : public IUri
 {
 public:
-    UriQt(const std::string& uri){this->pImpl_= new QUrl(uri.c_str());}
-    ~UriQt(){ if(nullptr != this->pImpl_) delete this->pImpl_;}
+    UriQt(const std::string& uri);
+    ~UriQt();
 
     virtual const std::string GetScheme() const override;
     virtual const std::string GetHost() const override;
@@ -26,7 +26,11 @@ public:
     virtual const std::string ToString()const override;
 
 private:
-    QUrl* pImpl_;
+    // QUrl* pImpl_;
+    std::string uri_;
+    std::string scheme_;
+    std::string host_;
+    int port_;
 };
 
 }}} // namespace rmscore { namespace platform { namespace http {
